91a6d5635794ed48d73720f47b3184a04a12b1c9
João Alves <joao.alves@ecosis.com.br>
2022-10-06 14:15:27 -0300

    Create CardList component

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+
+import { Card } from "../Card"
+import { styles } from "./styles"
+
+export function Cards() {
+    return (
+        <View style={styles.container}>
+            <Card />
+        </View>
+    )
+}
---
+++ src/components/Cards/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+
+        marginTop: 30,
+        marginBottom: 50,
+        marginLeft: 24,
+        marginRight: 24
+    },
+})
