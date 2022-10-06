bd6e0632ad89c7e24857727f92ccd234b04e0866
João Alves <joao.alves@ecosis.com.br>
2022-10-06 14:15:40 -0300

    Create Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,48 @@
+import { View, Text, Image } from "react-native"
+import { FontAwesome } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { THEME } from "../../theme"
+
+import { Cards } from "../../components/Cards"
+
+const AVATAR_URL =
+    "https://lh3.googleusercontent.com/a/ALm5wu38aAP6g2PkFVzpxiChguFyURQJgVdj9WL-Zmj04Q=s288-p-rw-no"
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+                <View style={styles.heading}>
+                    <View>
+                        <Text style={styles.welcomeText}>Bem vindo,</Text>
+                        <Text style={styles.usernameText}>João Pedro</Text>
+                    </View>
+
+                    <View style={styles.headingTools}>
+                        <View style={styles.headingNotification}>
+                            <View style={styles.badge} />
+                            <FontAwesome
+                                name="bell"
+                                style={styles.bell}
+                                size={THEME.FONT_SIZE.LG}
+                                color={THEME.COLORS.CAPTION_300}
+                            />
+                        </View>
+
+                        <View style={styles.avatar}>
+                            <Image
+                                source={{ uri: AVATAR_URL }}
+                                style={styles.avatarImage}
+                            />
+                        </View>
+                    </View>
+                </View>
+
+                <Cards />
+            </View>
+
+        </View>
+    )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native"
+import { THEME } from "../../theme"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: THEME.COLORS.BACKGROUND_PAPER,
+    },
+    heading: {
+        paddingHorizontal: 24,
+        paddingVertical: 8,
+
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    welcomeText: {
+        color: THEME.COLORS.CAPTION_300,
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        fontSize: THEME.FONT_SIZE.SM,
+    },
+    usernameText: {
+        color: THEME.COLORS.CAPTION_900,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.MD,
+    },
+
+    headingTools: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    headingNotification: {
+        position: "relative",
+        marginRight: 16,
+    },
+    badge: {
+        backgroundColor: THEME.COLORS.ALERT,
+        borderColor: THEME.COLORS.TEXT,
+        borderWidth: 1,
+        zIndex: 2,
+        position: "absolute",
+        right: 0,
+
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+    },
+    bell: {
+        zIndex: 1,
+    },
+    avatar: {
+        borderRadius: 20,
+        overflow: "hidden",
+    },
+    avatarImage: {
+        width: 40,
+        height: 40,
+    },
+})
