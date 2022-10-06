1842daac24fba065bbe5a142f38e70bf29f1035b
João Alves <joao.alves@ecosis.com.br>
2022-10-06 14:15:17 -0300

    Create card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,43 @@
+import { View, Text, Image } from "react-native"
+import { Feather } from "@expo/vector-icons"
+
+import paypal from "../../../assets/app/paypal-logo.png"
+import ellipse from "../../../assets/app/ellipse.png"
+import cardVector from "../../../assets/app/card-vector.png"
+
+import { styles } from "./styles"
+import { THEME } from "../../theme"
+
+export function Card() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.cardHeader}>
+                <View>
+                    <Text style={styles.title}>Balance</Text>
+                    <Text style={styles.subtitle}>Today, 08 Sept 2019</Text>
+                </View>
+
+                <View>
+                    <Feather
+                        name="more-horizontal"
+                        color={THEME.COLORS.TEXT}
+                        size={THEME.FONT_SIZE.LG}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.cardBody}>
+                <View>
+                    <Text style={styles.balance}>$ 6,240.00</Text>
+                    <Text style={styles.subtitle}>
+                        up by 2% from last month
+                    </Text>
+                </View>
+
+                <Image style={styles.paypal} source={paypal} />
+                <Image style={styles.ellipse} source={ellipse} />
+                <Image style={styles.cardVector} source={cardVector} />
+            </View>
+        </View>
+    )
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native"
+import { THEME } from "../../theme"
+
+export const styles = StyleSheet.create({
+    container: {
+        paddingTop: 32,
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingBottom: 16,
+        backgroundColor: THEME.COLORS.PRIMARY,
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    cardHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start'
+    },
+    title: {
+        color: THEME.COLORS.TEXT,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.MD
+    },
+    subtitle: {
+        color: THEME.COLORS.TEXT,
+        fontFamily: THEME.FONT_FAMILY.LIGHT,
+        fontSize: THEME.FONT_SIZE.SM
+    },
+    cardBody: {
+        position: 'relative',
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        marginTop: 32
+    },
+    balance: {
+        color: THEME.COLORS.TEXT,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.LG
+    },
+    paypal: {
+        width: 60,
+        height: 30
+    },
+    ellipse: {
+        position: 'absolute',
+        bottom: 120,
+        left: -20,
+        width: 60,
+        height: 60
+    },
+    cardVector: {
+        position: 'absolute',
+        bottom: -15,
+        right: -30
+    }
+})
