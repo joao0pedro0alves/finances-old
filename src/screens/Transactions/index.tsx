7a6ebd5515a8cba75038d91f5eecb61f71e95bb7
João Alves <joao.alves@ecosis.com.br>
2022-10-07 08:54:41 -0300

    Finish Transactions screen layout

--- src/screens/Transactions/index.tsx
+++ src/screens/Transactions/index.tsx
@@ -1,6 +1,10 @@
-import { View } from "react-native"
-import { BalanceHeading } from "../../components/BalanceHeading"
+import { View, Text, TouchableOpacity } from "react-native"
+import { MaterialIcons } from '@expo/vector-icons'
 
+import { BalanceHeading } from "../../components/BalanceHeading"
+import { TransactionList } from "../../components/TransactionList"
+
+import { THEME } from "../../theme"
 import { styles } from "./styles"
 
 export function Transactions() {
@@ -9,8 +13,33 @@ export function Transactions() {
 
             <BalanceHeading />
 
-            <View style={{flex: 2}}>
+            <View style={styles.listContainer}>
 
+                <View style={styles.listHeader}>
+
+                    <View style={styles.listFilter}>
+                        <Text style={[styles.listFilterText, styles.listFilterTextActive]}>
+                            Expenses
+                        </Text>
+                        <Text style={styles.listFilterText}>
+                            Earnings
+                        </Text>
+                    </View>
+
+                    <TouchableOpacity style={styles.listSorter}>
+                        <Text style={styles.listSorterText}>
+                            Sort by
+                        </Text>
+
+                        <MaterialIcons 
+                            name='arrow-drop-down'
+                            size={THEME.FONT_SIZE.LG}
+                            color={THEME.COLORS.CAPTION_900}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <TransactionList />
             </View>
             
         </View>
--- src/screens/Transactions/styles.ts
+++ src/screens/Transactions/styles.ts
@@ -1,7 +1,44 @@
 import { StyleSheet } from "react-native"
+import { THEME } from "../../theme"
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    listContainer: {
+        padding: 24,
+        flex: 2,
+    },
+    listHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listFilter: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listFilterText: {
+        color: THEME.COLORS.CAPTION_900,
+        fontSize: THEME.FONT_SIZE.SM,
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        marginRight: 16
+    },
+    listFilterTextActive: {
+        color: THEME.COLORS.PRIMARY,
+        fontSize: THEME.FONT_SIZE.SM,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+    },
+
+    listSorter: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listSorterText: {
+        color: THEME.COLORS.CAPTION_900,
+        fontSize: THEME.FONT_SIZE.SM,
+        fontFamily: THEME.FONT_FAMILY.SEMI_BOLD
+    }
 })
